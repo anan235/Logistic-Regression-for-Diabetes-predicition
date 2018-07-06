4d9f0f60fe813cfd7a445564bda4493e3c2dd54a
Ananay Batra <batraani@gmail.com>
2018-07-06 23:57:20 +0530

    First commit

---
+++ DiabetesPrediction.py
@@ -0,0 +1,41 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+ 
+from sklearn.linear_model import LogisticRegression
+from sklearn.externals import joblib
+
+diabetesDF = pd.read_csv('diabetes.csv')
+print(diabetesDF.head())
+
+diabetesDF.info()
+
+corr = diabetesDF.corr()
+print(corr)
+sns.heatmap(corr, 
+        xticklabels=corr.columns,
+        yticklabels=corr.columns)
+dfTrain = diabetesDF[:650]
+dfTest = diabetesDF[650:750]
+dfCheck = diabetesDF[750:] 
+
+trainLabel = np.asarray(dfTrain['Outcome'])
+trainData = np.asarray(dfTrain.drop('Outcome',1))
+testLabel = np.asarray(dfTest['Outcome'])
+testData = np.asarray(dfTest.drop('Outcome',1))
+
+means = np.mean(trainData, axis=0)
+stds = np.std(trainData, axis=0)
+ 
+trainData = (trainData - means)/stds
+testData = (testData - means)/stds
+
+diabetesCheck = LogisticRegression()
+diabetesCheck.fit(trainData, trainLabel)
+
+accuracy = diabetesCheck.score(testData, testLabel)
+print("accuracy = ", accuracy * 100, "%")
